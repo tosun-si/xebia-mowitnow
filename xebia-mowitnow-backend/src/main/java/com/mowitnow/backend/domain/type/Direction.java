a5854b0c8078d954d3d272d9c9a763b169ec3c86
mazizou <mazlum.tosun@gmail.com>
2016-10-08 16:51:58 +0200

    first commit

---
+++ xebia-mowitnow-backend/src/main/java/com/mowitnow/backend/domain/type/Direction.java
@@ -0,0 +1,15 @@
+package com.mowitnow.backend.domain.type;
+
+/**
+ * Contains all possibles mower directions.
+ * 
+ * @author Mazlum TOSUN
+ */
+public enum Direction {
+
+  // ----------------------------------------------
+  // Enumerations
+  // ----------------------------------------------
+
+  G, D, A
+}
